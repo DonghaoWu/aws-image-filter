a5f9267be2640a7a40aab201f1821f8bbe849b4a
Donghao Wu <donghao.wu.us@gmail.com>
2021-05-05 23:41:44 -0400

    add two routes.

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "imagepost",
+    "password": "imagepost",
+    "database": "postgres",
+    "host": "imagepost.c373g1a9htmv.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
-    "aws_region": "us-east-2",
+    "aws_region": "us-east-1",
     "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_media_bucket": "images-post-dev-v1-20210504"
   },
   "prod": {
     "username": "",
